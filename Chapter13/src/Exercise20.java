5a86eac73a7e5fdaa469098dc0211689eb8bde94
raywang999 <raywang999@gmail.com>
2018-12-27 10:36:23 -0500

    finish chapter 13 exercise 20

--- Chapter13/src/Exercise20.java
+++ Chapter13/src/Exercise20.java
@@ -7,15 +7,14 @@ Test your program with a standard library class and a class you create.
  */
 import static net.mindview.util.Print.*;
 import java.lang.annotation.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.lang.reflect.*;
+import java.util.regex.*;
 
 public class Exercise20 {
-    class InnerClass{}
     public static void superClasses(Class<?> c){
         Class superC = c.getSuperclass();
         if (superC!=null){
-            print(superC);
+            print("Extends: " + superC);
             superClasses(superC);
         }
     }
@@ -29,19 +28,78 @@ public class Exercise20 {
         try{
             c=Class.forName(args[0]);
         } catch (ClassNotFoundException e){
-            e.printStackTrace();
+            print("Class not found");
+            System.exit(0);
         }
         print("Your class: " + c);
         superClasses(c);
-        print("Annotation count: " + c.getAnnotations().length);
         print("Annotation list: ");
         for(Annotation a:c.getAnnotations())
-            print(a);
+            print("    " + a);
         print("Canonical Name: " + c.getCanonicalName());
         print("Inner classes and interfaces count: " + c.getClasses().length);
         print("Inner classes and interfaces list: ");
         for(Class myC: c.getClasses())
-            print(p.matcher(myC.toString()).replaceAll(""));
-        print(c.getClassLoader());
+            print("    " + p.matcher(myC.toString()).replaceAll(""));
+        print("Class loader: " + c.getClassLoader());
+        print("Component type: " + c.getComponentType());
+        print("List of constructors: ");
+        for (Constructor<?> con: c.getConstructors())
+            print("    " + con.getName());
+        print("Declared Annotation list: ");
+        for(Annotation a:c.getDeclaredAnnotations())
+            print("    " + a);
+        print("List of Declared classes: ");
+        for (Class<?> cl: c.getDeclaredClasses())
+            print("    "+cl.getName());
+        print("List of Declared constructors: ");
+        for (Constructor<?> con: c.getDeclaredConstructors())
+            print("    "+con);
+        print("List of Declared fields: ");
+        for (Field f: c.getDeclaredFields())
+            print("    "+f);
+        print("List of Declared methods: ");
+        for (Method m: c.getDeclaredMethods())
+            print("    "+m);
+        print(" Class:" + c.getClass());
+        print("Enclosing Class: " + c.getEnclosingClass());
+        print("Enclosing Constructor: " + c.getEnclosingConstructor());
+        print("Enclosing Method: " + c.getEnclosingMethod());
+        print("List of Enum constants: ");
+        try {
+            for (Object o : c.getEnumConstants())
+                print("    " + o);
+        } catch (NullPointerException e){
+            print("null");
+        }
+        print("List of fields: ");
+        for (Field f: c.getFields())
+            print("    "+f);
+        print("List of generic Interfaces: ");
+        for(Type t: c.getGenericInterfaces())
+            print("    " + t);
+        print("Direct superclass: " + c.getGenericSuperclass());
+        print("List of Interfaces");
+        for (Class<?> i: c.getInterfaces())
+            print("    " + i);
+        print("List of methods: ");
+        for (Method m: c.getMethods())
+            print("    "+m);
+        print("modifiers: " + c.getModifiers());
+        print("c.getName(): " + c.getName());
+        print("c.getPackage" + c.getPackage());
+        print("c.getProtectionDomain: " + c.getProtectionDomain());
+        print("List of signers: ");
+        try {
+            for (Object o : c.getSigners())
+                print("    " + o);
+        } catch (NullPointerException e){
+            print("null");
+        }
+        print("c.getSimpleName: " + c.getSimpleName());
+        print("Superclass of C: " + c.getSuperclass());
+        print("List of TypeVariables: ");
+        for (TypeVariable t: c.getTypeParameters())
+            print("    " + t);
     }
 }
--- Chapter13/src/Exercise20Test.java
+++ Chapter13/src/Exercise20Test.java
@@ -1,6 +1,13 @@
-interface i{}
+interface i{
+    void hi();
+}
 
-public class Exercise20Test implements i{
+class A{}
+
+public class Exercise20Test extends A implements i{
+    @Deprecated
+    int asdf;
+    double dsad;
     public interface innerI{}
     private class innerClass{}
     public innerClass get(){return new innerClass();}
@@ -10,4 +17,6 @@ public class Exercise20Test implements i{
         innerClass i = e.get();
         System.out.println(i);
     }
+    @Override
+    public void hi(){System.out.println("hi");}
 }
